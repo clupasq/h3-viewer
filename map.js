9dd67751de7f8f3584c81ae1262ef5e27f160d4f
Cristian Lupascu <cristi.lupascu@pm.me>
2021-07-25 19:01:01 +0300

    Map with H3 display

---
+++ map.js
@@ -0,0 +1,203 @@
+let map, hexLayer;
+
+const GeoUtils = {
+    EARTH_RADIUS_METERS: 6371000,
+
+    radiansToDegrees: (r) => r * 180 / Math.PI,
+    degreesToRadians: (d) => d * Math.PI / 180,
+
+    getDistanceOnEarthInMeters: (lat1, lon1, lat2, lon2) => {
+        const lat1Rad  = GeoUtils.degreesToRadians(lat1);
+        const lat2Rad  = GeoUtils.degreesToRadians(lat2);
+        const lonDelta = GeoUtils.degreesToRadians(lon2 - lon1);
+        const x = Math.sin(lat1Rad) * Math.sin(lat2Rad) +
+            Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.cos(lonDelta);
+        return GeoUtils.EARTH_RADIUS_METERS * Math.acos(Math.max(Math.min(x, 1), -1));
+    }
+};
+
+const ZOOM_TO_H3_RES_CORRESPONDENCE = {
+    3: 0,
+    4: 1,
+    5: 1,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 4,
+    10: 5,
+    11: 6,
+    12: 6,
+    13: 7,
+    14: 8,
+    15: 9,
+    16: 9,
+    17: 10,
+    18: 10,
+    19: 11,
+    20: 11,
+    21: 12,
+    22: 13,
+    23: 14,
+    24: 15,
+};
+
+const H3_RES_TO_ZOOM_CORRESPONDENCE = {};
+for (const [zoom, res] of Object.entries(ZOOM_TO_H3_RES_CORRESPONDENCE)) {
+    H3_RES_TO_ZOOM_CORRESPONDENCE[res] = zoom;
+}
+
+const getH3ResForMapZoom = (mapZoom) => {
+    return ZOOM_TO_H3_RES_CORRESPONDENCE[mapZoom] ?? Math.floor((mapZoom - 1) * 0.7);
+};
+
+const h3BoundsToPolygon = (lngLatH3Bounds) => {
+    const latLngBounds = lngLatH3Bounds.map(([lng, lat]) => [lat, lng]);
+    latLngBounds.push(latLngBounds[0]); // "close" the polygon
+    return latLngBounds;
+};
+
+/**
+ * Parse the current Query String and return its components as an object.
+ */
+const parseQueryString = () => {
+    const queryString = window.location.search;
+    const query = {};
+    const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (let i = 0; i < pairs.length; i++) {
+        const pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+};
+
+const queryParams = parseQueryString();
+
+const copyToClipboard = (text) => {
+    const dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+};
+
+var app = new Vue({
+    el: "#app",
+
+    data: {
+        searchH3Id: undefined,
+        gotoLatLon: undefined,
+        currentH3Res: undefined,
+
+    },
+
+    computed: {
+    },
+
+    methods: {
+
+        updateMapDisplay: function() {
+            if (hexLayer) {
+                hexLayer.remove();
+            }
+
+            hexLayer = L.layerGroup().addTo(map);
+
+            const zoom = map.getZoom();
+            this.currentH3Res = getH3ResForMapZoom(zoom);
+            const { _southWest: sw, _northEast: ne} = map.getBounds();
+
+            const boundsPolygon =[
+                [ sw.lng, sw.lat ],
+                [ sw.lng, ne.lat ],
+                [ ne.lng, ne.lat ],
+                [ ne.lng, sw.lat ],
+                [ sw.lng, sw.lat ],
+            ];
+
+            const h3s = h3.polyfill(boundsPolygon, this.currentH3Res);
+
+            for (const h3id of h3s) {
+
+                const polygonLayer = L.layerGroup()
+                    .addTo(hexLayer);
+
+                const isSelected = h3id === this.searchH3Id;
+
+                const style = isSelected ? { fillColor: "orange" } : {};
+
+                const h3Bounds = h3.h3ToGeoBoundary(h3id);
+                const h3Polygon = L.polygon(h3BoundsToPolygon(h3Bounds), style)
+                    .on('click', () => copyToClipboard(h3id))
+                    .bindTooltip(h3id)
+                    .addTo(polygonLayer);
+
+                // less SVG, otherwise perf is bad
+                if (Math.random() > 0.8 || isSelected) {
+                    var svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                    svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+                    svgElement.setAttribute('viewBox', "0 0 200 200");
+                    svgElement.innerHTML = `<text x="20" y="70" class="h3Text">${h3id}</text>`;
+                    var svgElementBounds = h3Polygon.getBounds();
+                    L.svgOverlay(svgElement, svgElementBounds).addTo(polygonLayer);
+                }
+            }
+        },
+
+        gotoLocation: function() {
+            const [lat, lon] = (this.gotoLatLon || "").split(",").map(Number);
+            if (Number.isFinite(lat) && Number.isFinite(lon)
+                && lat <= 90 && lat >= -90 && lon <= 180 && lon >= -180) {
+                map.setView([lat, lon], 16);
+            }
+        },
+
+        findH3: function() {
+            if (!h3.h3IsValid(this.searchH3Id)) {
+                return;
+            }
+            const h3Boundary = h3.h3ToGeoBoundary(this.searchH3Id);
+
+            let bounds = undefined;
+
+            for ([lng, lat] of h3Boundary) {
+                if (bounds === undefined) {
+                    bounds = new L.LatLngBounds([lat, lng], [lat, lng]);
+                } else {
+                    bounds.extend([lat, lng]);
+                }
+            }
+
+            map.fitBounds(bounds);
+
+            const newZoom = H3_RES_TO_ZOOM_CORRESPONDENCE[h3.h3GetResolution(this.searchH3Id)];
+            map.setZoom(newZoom);
+        }
+    },
+
+    beforeMount() {
+    },
+
+    mounted() {
+        document.addEventListener("DOMContentLoaded", () => {
+            map = L.map('mapid');
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                minZoom: 3,
+                maxNativeZoom: 19,
+                maxZoom: 24,
+                attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+            }).addTo(map);
+            pointsLayer = L.layerGroup([]).addTo(map);
+
+            const initialLat = queryParams.lat ?? 0;
+            const initialLng = queryParams.lng ?? 0;
+            const initialZoom = queryParams.zoom ?? 3;
+            map.setView([initialLat, initialLng], initialZoom);
+            map.on("zoomend", this.updateMapDisplay);
+            map.on("moveend", this.updateMapDisplay);
+
+            this.updateMapDisplay();
+        });
+    }
+});
